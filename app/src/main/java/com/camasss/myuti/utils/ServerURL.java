79053097bb103d80d2f6a9a57a46c516216e120e
Maksim <therundllone@gmail.com>
2021-10-07 18:46:05 +0300

    First commit

---
+++ app/src/main/java/com/camasss/myuti/utils/ServerURL.java
@@ -0,0 +1,13 @@
+package com.camasss.myuti.utils;
+
+public class ServerURL {
+
+    public static final String Url = "https://myutilities.ru/apis/load_data.php";
+    public static final String load_image = "https://myutilities.ru/public/uploads";
+
+
+    public static final String upload_files = "insertdata";
+    public static final String register_driver = "insertdriver";
+    public static final String BaseUrl = "https://myutilities.ru";
+
+}
